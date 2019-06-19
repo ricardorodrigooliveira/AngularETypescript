a717a1a7c14ac36da15e56eda2d765456a280fa8
Ricardo Oliveira <ricardorodrigo.oliveira@gmail.com>
2019-06-19 17:24:30 -0300

    Aula 39 - Criando o componente topo manualmente

---
+++ projetos/app1/src/app/topo/topo.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+ //decorar a classe - Encaminha para a classe os metadados
+@Component({
+  selector: 'app-topo', //da nome (rótulo) ao componente dentro da aplicação
+  templateUrl: './topo.component.html' //indica qual é o template que essa classe vai controlar
+})
+export class TopoComponent {}
