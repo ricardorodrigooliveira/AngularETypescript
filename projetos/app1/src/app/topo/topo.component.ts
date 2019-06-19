f727c6e7a425e1055546197166b6b5a5e168ad6a
Ricardo Oliveira <ricardorodrigo.oliveira@gmail.com>
2019-06-19 17:41:34 -0300

    Aula 41 - Component Styles

--- projetos/app1/src/app/topo/topo.component.ts
+++ projetos/app1/src/app/topo/topo.component.ts
@@ -3,11 +3,16 @@ import { Component } from '@angular/core'
  //decorar a classe - Encaminha para a classe os metadados
 @Component({
   selector: 'app-topo', //da nome (rótulo) ao componente dentro da aplicação
-  templateUrl: './topo.component.html' //indica qual é o template que essa classe vai controlar
+  templateUrl: './topo.component.html', //indica qual é o template que essa classe vai controlar
   // ou
   /*template: //usando crases é possível quebrar linhas e identar o código
             `<p>
                 Esse é o componente topo
             </p>`*/
+  /*styles: [` .exemplo {
+     color: red
+    } `]
+    ou */
+  styleUrls: ['./topo.component.css'] //pode ser passado mais de um estilo, basta separar por virgula
 })
 export class TopoComponent {}
