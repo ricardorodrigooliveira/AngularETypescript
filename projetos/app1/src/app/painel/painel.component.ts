320c7241ffc4101184a553617796e98e945d2f39
Ricardo Oliveira <ricardorodrigo.oliveira@gmail.com>
2019-06-25 19:00:09 -0300

    Aula 64 - App1 - Verificando resposta parte 2

--- projetos/app1/src/app/painel/painel.component.ts
+++ projetos/app1/src/app/painel/painel.component.ts
@@ -12,16 +12,32 @@ export class PainelComponent implements OnInit {
 
   public frases: Frase[] = FRASES;
   public instrucao: string = 'Traduza a frase:';
-  public resposta: string
+  public resposta: string;
+  public rodada: number = 0;
+  public rodadaFrase: Frase;
 
-  constructor() { console.log(this.frases); }
+  constructor() {
+    this.rodadaFrase = this.frases[this.rodada];
+    console.log(this.rodadaFrase);
+  }
 
   ngOnInit() {
   }
 
   public atualizaResposta(resposta: Event): void {
     this.resposta = ((<HTMLInputElement>resposta.target).value);
-    console.log(this.resposta);
+  }
+
+  public verificarResposta(): void {
+
+    if (this.rodadaFrase.frasePtBr == this.resposta) {
+      alert('A tradução está correta');
+      this.rodada++;
+      this.rodadaFrase = this.frases[this.rodada];
+    }else{
+      alert('A tradução está errada');
+    }
+
   }
 
 }
