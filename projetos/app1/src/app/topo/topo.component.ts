26b7e8cffeecdb5a9c389a8cab2b77675aaf6ef2
Ricardo Oliveira <ricardorodrigo.oliveira@gmail.com>
2019-06-25 15:12:07 -0300

    Até aula 53

--- projetos/app1/src/app/topo/topo.component.ts
+++ projetos/app1/src/app/topo/topo.component.ts
@@ -2,17 +2,32 @@ import { Component } from '@angular/core'
 
  //decorar a classe - Encaminha para a classe os metadados
 @Component({
-  selector: 'app-topo', //da nome (rótulo) ao componente dentro da aplicação
+  selector: 'app-topo', //seletor baseado em rotulo da nome (rótulo) ao componente dentro da aplicação
   templateUrl: './topo.component.html', //indica qual é o template que essa classe vai controlar
-  // ou
-  /*template: //usando crases é possível quebrar linhas e identar o código
-            `<p>
-                Esse é o componente topo
-            </p>`*/
-  /*styles: [` .exemplo {
-     color: red
-    } `]
-    ou */
   styleUrls: ['./topo.component.css'] //pode ser passado mais de um estilo, basta separar por virgula
 })
-export class TopoComponent {}
+
+export class TopoComponent {
+  public titulo: string = 'Aprendendo Inglês'; //string interpolation (one-way-binding)
+}
+
+
+/* Outros exemplos
+
+Como uma classe do componente <div>, cuidado para não usar o mesmo nome nas classes CSS
+@Component({
+  selector: '.app-topo', //da nome (rótulo) ao componente dentro da aplicação
+  template: '<p>Esse é o componente topo</p>'
+  styles: ['.exemplo {color: red} ']
+})
+
+ou
+
+//Como atributo ou rótulo do componente <div>
+@Component({
+  selector: '[app-topo]', //da nome (rótulo) ao componente dentro da aplicação
+  template: '<p>Esse é o componente topo</p>'
+  styles: ['.exemplo {color: red} ']
+})
+
+*/
