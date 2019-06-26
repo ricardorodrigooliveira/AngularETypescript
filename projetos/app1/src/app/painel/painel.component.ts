cf6ea54e9ac48cdc7d094a8490746be8cd59f865
Ricardo Oliveira <ricardorodrigo.oliveira@gmail.com>
2019-06-26 11:31:33 -0300

    Aula 68 - App1 - Criando a classe Coracao

--- projetos/app1/src/app/painel/painel.component.ts
+++ projetos/app1/src/app/painel/painel.component.ts
@@ -12,14 +12,13 @@ export class PainelComponent implements OnInit {
 
   public frases: Frase[] = FRASES;
   public instrucao: string = 'Traduza a frase:';
-  public resposta: string;
+  public resposta: string = '';
   public rodada: number = 0;
   public rodadaFrase: Frase;
   public progresso: number = 0;
 
   constructor() {
-    this.rodadaFrase = this.frases[this.rodada];
-    console.log(this.rodadaFrase);
+    this.atualizaRodada();
   }
 
   ngOnInit() {
@@ -31,15 +30,20 @@ export class PainelComponent implements OnInit {
 
   public verificarResposta(): void {
 
-    if (this.rodadaFrase.frasePtBr == this.resposta) {
+    if (this.rodadaFrase.frasePtBr === this.resposta) {
       alert('A tradução está correta');
       this.rodada++;
       this.progresso = this.progresso + (100 / this.frases.length);
-      this.rodadaFrase = this.frases[this.rodada];
+      this.atualizaRodada();
     }else{
       alert('A tradução está errada');
     }
 
+  }
+
+  public atualizaRodada(): void {
+    this.rodadaFrase = this.frases[this.rodada];
+    this.resposta = '';
   }
 
 }
--- projetos/app1/src/app/tentativas/tentativas.component.ts
+++ projetos/app1/src/app/tentativas/tentativas.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Coracao } from '../sharec/coracao.model.ts';
+
 @Component({
   selector: 'app-tentativas',
   templateUrl: './tentativas.component.html',
@@ -9,6 +11,12 @@ export class TentativasComponent implements OnInit {
 
   public coracaoVazio: string = '/assets/coracao_vazio.png'; //propert binding
   public coracaoCheio: string = '/assets/coracao_cheio.png';
+
+  public coracoes: Coracao[] = [
+    new Coracao(true),
+    new Coracao(true),
+    new Coracao(true)
+  ]
 
   constructor() { }
 
